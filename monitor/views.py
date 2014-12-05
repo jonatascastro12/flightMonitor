c57c23c94fce417196d124cf028f2167faa8949b
jonatascastro12 <jonatascastro12@gmail.com>
2014-12-05 15:50:05 -0200

    Envio de e-mail de alerta

--- monitor/views.py
+++ monitor/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+
+from django.core.mail import send_mail
 from monitor.models import Flight
 from monitor.serializers import FlightSerializer
 from rest_framework import generics
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ExtentListCreateAPIView(generics.ListCreateAPIView):
+    """
+    Concrete view for listing a queryset or creating a model instance.
+    """
+
+    def post(self, request, *args, **kwargs):
+    	flight_s = FlightSerializer(data=request.data)
+    	if (flight_s.is_valid()):
+    		flight = flight_s.save()
+    		if (flight.price < 500):
+    			print "ENVIA E-MAIL DE ALERTA"
+    			send_mail("Alerta de preço de passagem: ".decode('utf8')+flight.destination, flight.searchUrl, 'sistema@passagens.com.br',['jonatascastro12@gmail.com'])    		
+
+        return self.create(request, *args, **kwargs)
+
 
 # Create your views here.
-class FlightList(generics.ListCreateAPIView):
+class FlightList(ExtentListCreateAPIView):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
 
