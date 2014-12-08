9c15366f0e19b24b3e58dbe6bdabb9385b4bb085
jonatascastro12 <jonatascastro12@gmail.com>
2014-12-08 16:54:48 -0200

    alteracoes

--- monitor/models.py
+++ monitor/models.py
@@ -7,93 +7,98 @@ from django.forms.utils import flatatt, to_current_timezone
 from django.utils.safestring import mark_safe
 
 
-
 class BitChoices(object):
-	def __init__(self, choices):
-		self._choices = []
-		self._lookup = {}
-		for index, (key, val) in enumerate(choices):
-			index = 2**index
-			self._choices.append((index, val))
-			self._lookup[key] = index
+    def __init__(self, choices):
+        self._choices = []
+        self._lookup = {}
+        for index, (key, val) in enumerate(choices):
+            index = 2 ** index
+            self._choices.append((index, val))
+            self._lookup[key] = index
 
-	def __iter__(self):
-		return iter(self._choices)
+    def __iter__(self):
+        return iter(self._choices)
 
-	def __len__(self):
-		return len(self._choices)
+    def __len__(self):
+        return len(self._choices)
 
-	def __getattr__(self, attr):
-		try:
-			return self._lookup[attr]
-		except KeyError:
-			raise AttributeError(attr)
+    def __getattr__(self, attr):
+        try:
+            return self._lookup[attr]
+        except KeyError:
+            raise AttributeError(attr)
 
-	def get_selected_keys(self, selection):
-		""" Return a list of keys for the given selection """
-		return [ k for k,b in self._lookup.iteritems() if b & selection]
+    def get_selected_keys(self, selection):
+        """ Return a list of keys for the given selection """
+        return [k for k, b in self._lookup.iteritems() if b & selection]
 
-	def get_selected_values(self, selection):
-		""" Return a list of values for the given selection """
-		return [ b for b,v in self._choices if b & selection]
+    def get_selected_values(self, selection):
+        """ Return a list of values for the given selection """
+        return [b for b, v in self._choices if b & selection]
+
 
 WEEKDAY = BitChoices((('seg', 'Segunda'), ('ter', 'Terça'), ('qua', 'Quarta'),
-	('qui', 'Quinta'), ('sex', 'Sexta'), ('sab', 'Sábado'),
-        ('dom', 'Domingo')
-           ))      
+                      ('qui', 'Quinta'), ('sex', 'Sexta'), ('sab', 'Sábado'),
+                      ('dom', 'Domingo')
+))
 
 # Create your models here.
 class Flight(models.Model):
-	departDate = models.DateField()
-	returnDate = models.DateField()
-	origin = models.CharField(max_length=250)
-	destination = models.CharField(max_length=250, db_index=True)
-	searchUrl = models.CharField(max_length=600)
-	price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
-	timestamp =  models.DateTimeField(auto_now_add=True, blank=True)
+    departDate = models.DateField()
+    returnDate = models.DateField()
+    origin = models.CharField(max_length=250)
+    destination = models.CharField(max_length=250, db_index=True)
+    searchUrl = models.CharField(max_length=600)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+
 
 class FilterAlert(models.Model):
-	destinationFilter = models.CharField(max_length=250, blank=True, null=True)
-	email = models.EmailField()
-	maxPrice = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
-	dateStart = models.DateField(blank=True, null=True)
-	dateEnd = models.DateField(blank=True, null=True)
-	active = models.BooleanField(default=False)
-	weekday = models.PositiveIntegerField(default=0)
+    destinationFilter = models.CharField(max_length=250, blank=True, null=True)
+    email = models.EmailField()
+    maxPrice = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    dateStart = models.DateField(blank=True, null=True)
+    dateEnd = models.DateField(blank=True, null=True)
+    active = models.BooleanField(default=False)
+    weekday = models.PositiveIntegerField(default=0)
+
 
 class FlightAdmin(admin.ModelAdmin):
-	list_display = ('departDate','returnDate','destination','price','timestamp')
-	list_filter = ['destination', 'departDate','timestamp']
+    list_display = ('departDate', 'returnDate', 'destination', 'price', 'timestamp')
+    list_filter = ['destination', 'departDate', 'timestamp']
+
 
 class BinaryWidget(forms.CheckboxSelectMultiple):
-	def render(self, name, value, attrs=None, choices=()):
-		if value is None:
-	        	value = 0
-		choices = self.choices
-		final_attrs = self.build_attrs(attrs, name=name)
-		output = [format_html('<select multiple="multiple"{0}>', flatatt(final_attrs))]
-	
-		value = WEEKDAY.get_selected_values(value)
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = 0
 
-		options = self.render_options(choices, value)
-		if options:
-	        	output.append(options)
-		output.append('</select>')
-	        return mark_safe('\n'.join(output))
+        final_attrs = self.build_attrs(attrs, name=name)
+        output = [format_html('<select multiple="multiple"{0}>', flatatt(final_attrs))]
 
-	def value_from_datadict(self, data, files, name):
-        	value = super(BinaryWidget, self).value_from_datadict(data, files, name)
-	        if name == 'weekday':
-			value = sum(int(i) for i in value)
-		return value
+        value = WEEKDAY.get_selected_values(value)
+
+        options = self.render_options([], value)
+        if options:
+            output.append(options)
+        output.append('</select>')
+        return mark_safe('\n'.join(output))
+
+    def value_from_datadict(self, data, files, name):
+        value = super(BinaryWidget, self).value_from_datadict(data, files, name)
+        if name == 'weekday':
+            value = sum(int(i) for i in value)
+        return value
 
 
 class FilterAlertForm(forms.ModelForm):
-	weekday = forms.IntegerField(widget=BinaryWidget(choices=WEEKDAY))
-	class Meta:
-		model = FilterAlert
+    weekday = forms.IntegerField(widget=BinaryWidget(choices=WEEKDAY))
+
+    class Meta:
+        model = FilterAlert
+
 
 class FilterAlertAdmin(admin.ModelAdmin):
-	form = FilterAlertForm
-	list_display = ('email', 'maxPrice', 'destinationFilter', 'active')
-	
+    form = FilterAlertForm
+    list_display = ('email', 'maxPrice', 'destinationFilter', 'active')
+    
--- monitor/views.py
+++ monitor/views.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from django.core.mail import send_mail
-from monitor.models import Flight, FilterAlert
+from monitor.models import Flight, FilterAlert, WEEKDAY
 from monitor.serializers import FlightSerializer
 from rest_framework import generics
 import logging
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
+import math
 
 
 logger = logging.getLogger(__name__)
 alerts = FilterAlert.objects.filter(active=True)
 
+
 def updateAlerts():
-	alerts = FilterAlert.objects.filter(active=True)
-	return HttpResponse("OK")
+    alerts = FilterAlert.objects.filter(active=True)
+    return HttpResponse("OK")
+
 
 class ExtentListCreateAPIView(generics.ListCreateAPIView):
     """
@@ -23,28 +26,43 @@ class ExtentListCreateAPIView(generics.ListCreateAPIView):
     """
 
     def post(self, request, *args, **kwargs):
-    	flight_s = FlightSerializer(data=request.data)
-	filter_date = True
-	filter_price = True
-	filter_destination = True
-    	if (flight_s.is_valid()):
-		for alert in alerts:
-			if flight_s.validated_data.get('price') <= alert.maxPrice:
-				filter_price = True
-			if alert.dateStart and alert.dateEnd:
-				filter_date = False
-				if flight_s.validated_data.get('departDate') >= alert.dateStart and flight_s.validated_data.get('departDate') <= alert.dateEnd:
-					filter_date = True
-			if alert.destinationFilter:
-				filter_destination = False
-				if flight_s.validated_data.get('destination') == alert.destinationFilter:
-					filter_destination = True	   		
-			if filter_date and filter_price and filter_destination:
-				send_mail("Alerta de preço de passagem: ".decode('utf8')+flight_s.validated_data.get('destination'),"PREÇO: ".decode('utf8') + str(flight_s.validated_data.get('price')) + "\n"+ flight_s.validated_data.get('searchUrl'), 'sistema@passagens.com.br',[alert.email])
-	
-		if flight_s.validated_data.get('price') < 2000:
-			return self.create(request, *args, **kwargs)
-		return Response({"message": "Price higher than 2000.00"},status=status.HTTP_202_ACCEPTED)
+        flight_s = FlightSerializer(data=request.data)
+        filter_date = True
+        filter_price = True
+        filter_destination = True
+        filter_weekday = True
+        if (flight_s.is_valid()):
+            for alert in alerts:
+                if flight_s.validated_data.get('price') <= alert.maxPrice:
+                    filter_price = True
+                if alert.dateStart and alert.dateEnd:
+                    filter_date = False
+                    if flight_s.validated_data.get('departDate') >= alert.dateStart and flight_s.validated_data.get(
+                            'departDate') <= alert.dateEnd:
+                        filter_date = True
+                if alert.destinationFilter:
+                    filter_destination = False
+                    if flight_s.validated_data.get('destination') == alert.destinationFilter:
+                        filter_destination = True
+                if alert.weekday and True:
+                    filter_weekday = False
+                    days_of_week = [int(math.log(i, 2)) for i in WEEKDAY.get_selected_values(alert.weekday)]
+
+                    if flight_s.validated_data.get('departDate').weekday() in days_of_week:
+                        filter_weekday = True
+
+                if filter_date and filter_price and filter_destination and filter_weekday:
+                    send_mail(
+                        "Alerta de preço de passagem: ".decode('utf8') + flight_s.validated_data.get('destination'),
+                        "PREÇO: ".decode('utf8') + str(
+                            flight_s.validated_data.get('price')) + "\n" + flight_s.validated_data.get('searchUrl') + "\n" +
+                            str(flight_s.validated_data.get('departDate')),
+                        'sistema@passagens.com.br', [alert.email])
+
+            if flight_s.validated_data.get('price') < 2000:
+                return self.create(request, *args, **kwargs)
+            return Response({"message": "Price higher than 2000.00"}, status=status.HTTP_202_ACCEPTED)
+
 
 # Create your views here.
 class FlightList(ExtentListCreateAPIView):
